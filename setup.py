a893c2aa08b9689d00382cdf3d1de1cb7f478767
WataruTakahagi <watarut@outlook.com>
2019-07-11 21:35:30 +0900

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name='jcmc2csv',
+    version='0.1.2',
+    author='Wataru Takahagi',
+    author_email='watarut@eqchem.s.u-tokyo.ac.jp',
+    url='https://github.com/WataruTakahagi/jcmc',
+    py_modules=['jcmc'],
+    description='Collect information from JCMC',
+    keywords='microbiology, data-science',
+    classifiers=[
+        'Intended Audience :: Science/Research',
+        'Intended Audience :: Developers',
+        'Topic :: Scientific/Engineering :: Chemistry',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.7',
+    ],
+)
